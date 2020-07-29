8e78438a74248e3d087eda786023bdcc8cac43e5
Sanskaar Patni <patnisanskaar@gmail.com>
2020-07-29 14:36:07 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ const app = express();
 //allow cross origin requests
 app.use(cors());
 
-const url = 'mongodb://Sanskaar:shanx123@cluster0-shard-00-00.ixnix.mongodb.net:27017,cluster0-shard-00-01.ixnix.mongodb.net:27017,cluster0-shard-00-02.ixnix.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-19rj74-shard-0&authSource=admin&retryWrites=true&w=majority';
+const url = 'your_mongo_url';
 mongoose
     .connect(url, {
         useNewUrlParser: true,
